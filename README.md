1aa77aa03cffb764f1974e08cb74069c4156a6d8
Brian Sullender <b.sullender@gmail.com>
2023-09-22 10:40:43 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
-# expression-parser
-An expression parser in C that can easily be modified for more operations.
+# Expression-Parser
+
+Expression Parser is a versatile C-based parser designed for efficiency and speed, making it suitable for a wide range of applications. It supports various operations, both arithmetic and bitwise, and can be extended to include additional features.
+
+## Supported Operations
+
+The Expression Parser currently supports the following operations:
+
+### Arithmetic Operations
+
+- Addition `+`
+- Subtraction `-`
+- Multiplication `*`
+- Division `/`
+- Modulo `%`
+
+### Bitwise Operations
+
+- Left Shift `<<`
+- Right Shift `>>`
+- Bitwise AND `&`
+- Bitwise OR `|`
+- Bitwise XOR `^`
+
+You can use parentheses to control the order of operations and ensure precise calculations.
+
+LICENSE TERMS
+=============
+```
+This software is provided 'as-is', without any express or implied
+warranty.  In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+(1) If any part of the source code or algorithm is used, it is requested that
+    appropriate attribution is given to the authors by providing a link to
+    the original repository or a reference to the authors name.
+(2) Permission for use of this software is granted only if the user accepts
+    full responsibility for any undesirable consequences; the authors accept
+    NO LIABILITY for damages of any kind.
+
+By using this software, you acknowledge that you have read and understood
+these terms and agree to abide by them.
+```
+
+To compile and run the expression parser, follow these steps:
+
+1. Clone the repository to your local machine:
+   ```shell
+   git clone https://github.com/b-sullender/expression-parser.git
+   ```
+
+2. Navigate to the project directory:
+   ```shell
+   cd expression-parser
+   ```
+
+3. Compile the application using GCC:
+   ```shell
+   gcc main.c -o exp
+   ```
+
+4. Run the program:
+   ```shell
+   ./exp
+   ```
+
+## Extending the Parser
+
+One of the key strengths of the Expression Parser is its extensibility. You can easily add support for more operations by following these guidelines:
+
+1. **Define the Operation**: Add the name of the new operation to the `TokenType` enum. This enum defines the types of tokens that the parser can recognize.
+
+2. **Token Handling**: In the `getNextToken` function, add an `else if` handler for your new token type and set the appropriate precedence. This step ensures that the parser recognizes the new token correctly.
+
+3. **Operation Implementation**: In the `parseExpression` function, add an `else if` handler for your new token type. Here, you'll implement the actual operation associated with the new token type.
+
+Feel free to experiment and expand the capabilities of the Expression Parser to meet your specific needs or contribute to its development.
+
---
+++ main.c
@@ -0,0 +1,220 @@
+//
+// Copyright (c) 2023 Brian Sullender
+// All rights reserved.
+//
+// This source code is licensed under the terms provided in the README file.
+//
+// https://github.com/b-sullender/expression-parser
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+// Define token types
+typedef enum {
+    NUMBER,
+    PLUS,
+    MINUS,
+    MULTIPLY,
+    DIVIDE,
+    REMAINDER,
+    BIT_AND,
+    BIT_OR,
+    BIT_XOR,
+    BIT_LSHIFT,
+    BIT_RSHIFT,
+    LPAREN,
+    RPAREN,
+    END
+} TokenType;
+
+// Define token structure
+typedef struct {
+    TokenType type;
+    int precedence;
+    int value;
+} Token;
+
+// Define parser context
+typedef struct {
+    Token token;
+    char* input;
+} ParserContext;
+
+// Function to get the next token
+void getNextToken(ParserContext* context)
+{
+    char c = *context->input++;
+    while (c == ' ' || c == '\r' || c == '\n' || c == '\t') {
+        c = *context->input++;
+    }
+
+    if (c == '\0') {
+        context->token.type = END;
+        context->token.precedence = 0;
+        context->input--;
+        return;
+    }
+
+    if (c >= '0' && c <= '9') {
+        context->token.type = NUMBER;
+        context->token.precedence = 0;
+        context->token.value = c - '0';
+        c = *context->input++;
+        while (c >= '0' && c <= '9') {
+            context->token.value = context->token.value * 10 + (c - '0');
+            c = *context->input++;
+        }
+        context->input--;
+    } else if (c == '+') {
+        context->token.type = PLUS;
+        context->token.precedence = 700;
+    } else if (c == '-') {
+        context->token.type = MINUS;
+        context->token.precedence = 700;
+    } else if (c == '*') {
+        context->token.type = MULTIPLY;
+        context->token.precedence = 800;
+    } else if (c == '/') {
+        context->token.type = DIVIDE;
+        context->token.precedence = 800;
+    } else if (c == '%') {
+        context->token.type = REMAINDER;
+        context->token.precedence = 800;
+    } else if (c == '&') {
+        context->token.type = BIT_AND;
+        context->token.precedence = 500;
+    } else if (c == '|') {
+        context->token.type = BIT_OR;
+        context->token.precedence = 300;
+    } else if (c == '^') {
+        context->token.type = BIT_XOR;
+        context->token.precedence = 400;
+    } else if (c == '<' && *(context->input) == '<') {
+        context->token.type = BIT_LSHIFT;
+        context->token.precedence = 600;
+        context->input++;
+    } else if (c == '>' && *(context->input) == '>') {
+        context->token.type = BIT_RSHIFT;
+        context->token.precedence = 600;
+        context->input++;
+    } else if (c == '(') {
+        context->token.type = LPAREN;
+        context->token.precedence = 900;
+    } else if (c == ')') {
+        context->token.type = RPAREN;
+        context->token.precedence = 900;
+    }
+}
+
+// Forward declaration
+int parseExpression(ParserContext* context, int minPrecedence);
+
+// Function to parse a factor
+int parseFactor(ParserContext* context)
+{
+    int value;
+
+    if (context->token.type == NUMBER) {
+        value = context->token.value;
+        getNextToken(context);
+    } else if (context->token.type == LPAREN) {
+        getNextToken(context);
+        value = parseExpression(context, context->token.precedence);
+        if (context->token.type != RPAREN) {
+            fprintf(stderr, "Error: Missing closing parenthesis\n");
+            exit(EXIT_FAILURE);
+        }
+        getNextToken(context);
+    } else {
+        fprintf(stderr, "Error: Unexpected token\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
+
+// Function to parse an expression
+int parseExpression(ParserContext* context, int minPrecedence)
+{
+    // Get the factor
+    int value = parseFactor(context);
+
+    // Check that we can continue processing this term
+    while ((context->token.type != END) && (context->token.type != RPAREN) && (context->token.precedence > minPrecedence))
+    {
+        // Check if we have a valid operator token
+        if (context->token.type == NUMBER) {
+            fprintf(stderr, "%s\n", "Error: expected operator, not variable");
+            exit(EXIT_FAILURE);
+        }
+
+        // Save current token
+        Token token = context->token;
+
+        // Get the factor/term to the right
+        getNextToken(context);
+        int term = parseExpression(context, token.precedence);
+
+        // Perform the operation
+        if (token.type == PLUS) {
+            value += term;
+        } else if (token.type == MINUS) {
+            value -= term;
+        } else if (token.type == MULTIPLY) {
+            value *= term;
+        } else if (token.type == DIVIDE) {
+            value /= term;
+        } else if (token.type == REMAINDER) {
+            value %= term;
+        } else if (token.type == BIT_LSHIFT) {
+            value <<= term;
+        } else if (token.type == BIT_RSHIFT) {
+            value >>= term;
+        } else if (token.type == BIT_AND) {
+            value &= term;
+        } else if (token.type == BIT_XOR) {
+            value ^= term;
+        } else if (token.type == BIT_OR) {
+            value |= term;
+        }
+    }
+
+    // Return the result of the term
+    return value;
+}
+
+int main()
+{
+    char expre[256];
+
+    printf("%s\n", "Enter 'q' to exit");
+    while (true)
+    {
+        printf("Enter expression: ");
+        if (fgets(expre, 255, stdin) != expre) {
+            perror("Error reading expression\n");
+            return EXIT_FAILURE;
+        }
+        if (strcmp(expre, "q\n") == 0) {
+            return EXIT_SUCCESS;
+        }
+
+        char* pInput = expre;
+        ParserContext context;
+        context.input = pInput;
+
+        getNextToken(&context);
+        int result = parseExpression(&context, context.token.precedence);
+        if (context.token.type != END) {
+            fprintf(stderr, "%s\n", "Error: Unexpected token\n");
+            continue;
+        }
+        printf("Result: %d\n", result);
+    }
+
+    return EXIT_SUCCESS;
+}
+
